9d635013daa5c73c2961599647e0d47128659416
Bryan <bryanfonsecafernandes@gmail.com>
2025-09-11 23:52:01 +0000

    Dados iniciais das cartas.

---
+++ xadrez.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // ===== Dados iniciais das cartas =====
+    char estado1[3] = "CE";
+    char codigo1[5] = "C01";
+    char nomeCidade1[50] = "Fortaleza";
+    unsigned long int populacao1 = 2703391;
+    float area1 = 314.93;
+    float pib1 = 89.0;
+    int pontosTuristicos1 = 40;
+
+    char estado2[3] = "PA";
+    char codigo2[5] = "P01";
+    char nomeCidade2[50] = "Belem";
+    unsigned long int populacao2 = 1506420;
+    float area2 = 1059.46;
+    float pib2 = 47.4;
+    int pontosTuristicos2 = 30;
+
+  
