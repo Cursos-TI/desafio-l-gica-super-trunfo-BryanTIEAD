3b53e260465a16fd91062c2c9e0342978762e8c8
Bryan <bryanfonsecafernandes@gmail.com>
2025-09-29 23:37:32 +0000

    Comparaçao segundo atributo

--- desafiomestre.c
+++ desafiomestre.c
@@ -89,3 +89,33 @@ int main() {
             printf("Densidade -> Brasil: %d | Argentina: %d\n", densidade1, densidade2);
             break;
     }
+    // Comparação do segundo atributo
+    switch (escolha2) {
+        case 1:
+            resultado2 = (ataque1 > ataque2) ? 1 : (ataque2 > ataque1) ? 2 : 0;
+            soma1 += ataque1;
+            soma2 += ataque2;
+            printf("Ataque -> Brasil: %d | Argentina: %d\n", ataque1, ataque2);
+            break;
+        case 2:
+            resultado2 = (defesa1 > defesa2) ? 1 : (defesa2 > defesa1) ? 2 : 0;
+            soma1 += defesa1;
+            soma2 += defesa2;
+            printf("Defesa -> Brasil: %d | Argentina: %d\n", defesa1, defesa2);
+            break;
+        case 3:
+            resultado2 = (recuo1 > recuo2) ? 1 : (recuo2 > recuo1) ? 2 : 0;
+            soma1 += recuo1;
+            soma2 += recuo2;
+            printf("Recuo -> Brasil: %d | Argentina: %d\n", recuo1, recuo2);
+            break;
+        case 4:
+            resultado2 = (densidade1 < densidade2) ? 1 : (densidade2 < densidade1) ? 2 : 0;
+            soma1 += densidade1;
+            soma2 += densidade2;
+            printf("Densidade -> Brasil: %d | Argentina: %d\n", densidade1, densidade2);
+            break;
+        default:
+            printf("Opcao invalida!\n");
+            return 0;
+    }
