995cd41599dcb0e6320117d20eaf9745d87b2fda
Bryan <bryanfonsecafernandes@gmail.com>
2025-09-29 23:32:40 +0000

    Dados prescritos.

---
+++ desafiomestre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int resultado1, resultado2;
+    int ataque1, ataque2, defesa1, defesa2, recuo1, recuo2, densidade1, densidade2;
+    int soma1 = 0, soma2 = 0;
+    int escolha1, escolha2;
+        srand(time(0));
+
+   
