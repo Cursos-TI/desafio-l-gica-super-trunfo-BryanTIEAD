b49e3aeca3a65a85f05d77f643a55a62d9db150e
Bryan <bryanfonsecafernandes@gmail.com>
2025-09-29 23:34:09 +0000

    Atributo das cartas digitado

--- desafiomestre.c
+++ desafiomestre.c
@@ -9,4 +9,18 @@ int main() {
     int escolha1, escolha2;
         srand(time(0));
 
-   
+    // Atributos das cartas
+    ataque1 = rand() % 100 + 1;
+    ataque2 = rand() % 100 + 1;
+    defesa1 = rand() % 100 + 1;
+    defesa2 = rand() % 100 + 1;
+    recuo1 = rand() % 100 + 1;
+    recuo2 = rand() % 100 + 1;
+    densidade1 = rand() % 100 + 1;
+    densidade2 = rand() % 100 + 1;
+
+    printf("=== SUPER TRUNFO ===\n");
+    printf("Carta 1 (Brasil)\n");
+    printf("Ataque: %d | Defesa: %d | Recuo: %d | Densidade: %d\n", ataque1, defesa1, recuo1, densidade1);
+    printf("Carta 2 (Argentina)\n");
+    printf("Ataque: %d | Defesa: %d | Recuo: %d | Densidade: %d\n", ataque2, defesa2, recuo2, densidade2);
